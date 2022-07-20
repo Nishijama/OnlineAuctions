67ebea5e0805b1a1b179e512816aae31b5e07040
Nishijama <nathanplociennik@gmail.com>
2022-07-20 21:40:09 +0200

    Tried to implement image field, to be continued!

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -23,6 +23,7 @@ class Listing(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     start_date = models.DateField(auto_now_add=True, editable=False)
     end_date = models.DateField()
+    item_image = models.ImageField(upload_to ='uploads/', default="")
     
     def __str__(self):
         return self.title
