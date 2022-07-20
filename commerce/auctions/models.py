4e7b582d728833f097b550efcc2f75c11c8b48a7
Nishijama <nathanplociennik@gmail.com>
2022-07-20 21:02:21 +0200

    Included price field with complete validation

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator, DecimalValidator
 from django.db import models
 
 
@@ -19,6 +20,9 @@ class Listing(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
-
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    start_date = models.DateField(auto_now_add=True, editable=False)
+    end_date = models.DateField()
+    
     def __str__(self):
         return self.title
